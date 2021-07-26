cdd4423ffced022d8a4c599aab931eae62fb70ed
Rostislav <fadenougat@gmail.com>
2021-07-26 13:06:08 +0300

    Добавлено логирование

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python3
+from os import system
+from os.path import exists
+import datetime
+from csv import DictWriter, DictReader
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/91.0.4472.114 Safari/537.36'
 }
 
 
-__version__ = 'v0.1.3'
+__version__ = 'v0.1.4'
+
+
+FIELDS_LOG_FILE = ['version', 'date', 'cause', 'status']
+FILE_LOG = 'file.log'
 
 
 def get_articles():
+    """ Сбор артикулов в файл """
     file_with_link = 'link.txt'
     file_with_articles = 'articles.txt'
     if exists(file_with_link) is False:
-        with open(file_with_link, 'a') as file:
-            file.write('')
-            file.close()
-        print("Откройте файл \'" + file_with_link + "\' и добавьте ссылку WB")
+        try:
+            write_log('Добавление ссылки', 'Run')
+            print('\nСсылка сохранится')
+            while True:
+                link = input('Введите ссылку WB: ')
+                if link == '':
+                    print('Поле ввода не должно быть пустым')
+                    write_log('Пустое поле для ссылки', 'Fail')
+                else:
+                    break
+            with open(file_with_link, 'a') as file:
+                file.write(link)
+                file.close()
+            write_log('Ссылка добавлена', 'OK')
+        except Exception as error_else:
+            write_log(error_else, 'Fail')
 
     if exists(file_with_articles) is False:
         with open(file_with_articles, 'a') as file:
@@ -25,6 +47,7 @@ def get_articles():
         with open(file_with_articles, 'w') as file:
             file.write('')
             file.close()
+        write_log('Перезапись артикулов', 'OK')
 
     url_main = open(file_with_link).readline()
     s = 0
@@ -55,19 +78,47 @@ def get_articles():
                     system('clear')
                 print('Собрано артикулов: ', s)
         url_main = url_main.replace("page=" + str(page_now), "page=" + str(page_now + 1))
-    input("\n\n Press \'Enter\'")
+    write_log('Успешно', 'OK')
+    input("\n\n Нажмите \'Enter\'")
+
+
+def write_log(cause, status_itself):
+    """ Логирование """
+    def get_time_now():
+        hms = datetime.datetime.today()
+        time_format = str(hms.hour) + ':' + str(hms.minute) + ':' + str(hms.second)
+        date_format = str(hms.day) + '.' + str(hms.month) + '.' + str(hms.year)
+        total = str(time_format) + '-' + str(date_format)
+        return ''.join(total)
+
+    if exists(FILE_LOG) is False:
+        with open(FILE_LOG, mode="a", encoding='utf-8') as data:
+            logs_writer = DictWriter(data, fieldnames=FIELDS_LOG_FILE, delimiter=';')
+            logs_writer.writeheader()
+
+    log_data = open(FILE_LOG, mode="a", encoding='utf-8')
+    log_writer = DictWriter(log_data, fieldnames=FIELDS_LOG_FILE, delimiter=';')
+    log_writer.writerow({
+        FIELDS_LOG_FILE[0]: __version__,     # Запись версии
+        FIELDS_LOG_FILE[1]: get_time_now(),  # Запись даты и времени
+        FIELDS_LOG_FILE[2]: cause,           # Запись причины
+        FIELDS_LOG_FILE[3]: status_itself    # Запись статуса
+    })
 
 
 if __name__ == '__main__':
     try:
         from bs4 import BeautifulSoup
         from requests import get as get_req
-        from os import system
-        from os.path import exists
         from sys import platform as _platform
+        write_log('Запуск программы', 'Run')
         get_articles()
+        write_log('Exit', 'OK')
     except ModuleNotFoundError as error:
+        write_log(error, 'Fail')
         error = str(error)
         print('Установите отсутствующий/отсутствующие модуль/модули', error[15:], 'через PIP')
         print('\n Для установки всех отсутвующих модулей, введите команду в терминале:')
         print('pip3 install -r requirements.txt')
+    except Exception as error_r:
+        write_log(error_r, 'FAIL')
