adc9771c6d5779454479f5a2b3d70268ae82e5f8
Rostislav <fadenougat@gmail.com>
2021-07-26 11:49:49 +0300

    Update

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# -*- coding: UTF-8 -*-
-from bs4 import BeautifulSoup
-import requests
-import os
-from time import sleep
-
-
+#!/usr/bin/env python3
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/91.0.4472.114 Safari/537.36'
 }
 
 
-__version__ = 'v0.1.0'
+__version__ = 'v0.1.1'
 
 
 def get_articles():
@@ -28,42 +22,46 @@ def get_articles():
             file.close()
 
     url_main = open(file_with_link).readline()
-    s = progress = 0
-    cnt_page = 1
-    for page_now in range(10):
+    s = 0
+
+    for page_now in range(1, 5):
+        mas_articles = []
         page = requests.get(url_main)
         soup = BeautifulSoup(page.content, 'html.parser')
-        for line in soup.findAll('a', href=True):
-            if "detail.aspx?" in line["href"]:
-                progress += 1
-                if progress % 100 == 0:
-                    url_main = url_main.replace("page=" + str(cnt_page), "page=" + str(cnt_page + 1))
-                    cnt_page += 1
 
         for line in soup.findAll('a', href=True):
             if "detail.aspx?" in line["href"]:
-                if progress > 0:
-                    s += 1
-                    article_url = line['href']
-                    article_url = article_url[9::]
-                    article = ''
-                    for number in article_url:
-                        if number == '/':
-                            break
-                        else:
-                            article += number
-                    print(str(s) + ':', article)
-                    with open(file_with_articles, 'a') as articles_file:
-                        articles_file.write(article)
-                        articles_file.write('\n')
-                    print((s * 100) // progress, '%')
-                    if s % 100 == 0:
-                        url_main = url_main.replace("page=" + str(cnt_page), "page=" + str(cnt_page + 1))
-                        cnt_page += 1
-                else:
-                    print('Нет данных для сбора')
-    # input('\n\n OK')
+                s += 1
+                article_url = line['href']
+                article_url = article_url[9::]
+                article = ''
+                for number in article_url:
+                    if number == '/':
+                        break
+                    else:
+                        article += number
+                mas_articles.append(article)
+                with open(file_with_articles, 'a') as articles_file:
+                    articles_file.write(article)
+                    articles_file.write('\n')
+                if _platform == "darwin":
+                    os.system("clear && printf '\e[3J'")
+                elif _platform == 'linux':
+                    os.system('clear')
+                print('Собрано: ', s)
+        url_main = url_main.replace("page=" + str(page_now), "page=" + str(page_now + 1))
+    input("\n\n Press \'Enter\'")
 
 
 if __name__ == '__main__':
-    get_articles()
+    try:
+        from bs4 import BeautifulSoup
+        import requests
+        import os
+        from sys import platform as _platform
+        get_articles()
+    except ModuleNotFoundError as error:
+        error = str(error)
+        print('Установите отсутствующий модуль', error[15:], 'через PIP')
+        print('Для этого нужно ввести команду в терминале:')
+        print('\n python -m pip install -r requirements.txt')
